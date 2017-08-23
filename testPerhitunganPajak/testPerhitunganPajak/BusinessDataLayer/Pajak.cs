a8943e16d302d29c8faa2a5f6d82de62f59fd5aa
swtmath <aurum@DESKTOP-TR7G8NK>
2017-08-23 21:15:06 +0700

    23 agustus jam 21

---
+++ testPerhitunganPajak/testPerhitunganPajak/BusinessDataLayer/Pajak.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testPerhitunganPajak.BusinessDataLayer
+{
+    class Pajak
+    {
+        public static decimal CountBruto(DataAccessLayer.SalaryEmployeePayment salaryEmployeePayment)
+        {
+            decimal bruto = 0;
+			bruto += salaryEmployeePayment.RBasicSalary.Value;
+			bruto += salaryEmployeePayment.RAllowance.Value;
+			bruto -= salaryEmployeePayment.RDeduction.Value;
+			bruto += salaryEmployeePayment.ROverTime.Value;
+			bruto += salaryEmployeePayment.RJKM.Value;
+			bruto += salaryEmployeePayment.RJKK.Value;
+			bruto += salaryEmployeePayment.RBPJSComp.Value;
+			bruto += salaryEmployeePayment.TunjPPH21Emp.Value;
+			return bruto;
+        }
+		public static decimal CountBrutoListPayment (List<DataAccessLayer.SalaryEmployeePayment> listPayment)
+		{
+			decimal sumBruto = 0;
+			foreach(var a in listPayment)
+			{
+				sumBruto += CountBruto(a);
+			}
+			return sumBruto;
+		}
+    }
+}
---
+++ testPerhitunganPajak/testPerhitunganPajak/BusinessDataLayer/PajakTypeData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testPerhitunganPajak.BusinessDataLayer
+{
+    class PajakTypeData
+    {
+        enum TaxCalculationMethod
+        {
+            Annually,
+            Forecast,
+            WeightedAverage
+        };
+    }
+}
